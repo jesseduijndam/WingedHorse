859bc5263192c088aef2c4ad2a3426db02940bbb
jesseduijndam <50326150+jesseduijndam@users.noreply.github.com>
2019-06-11 15:08:13 +0200

    Delete eyes.ts

--- dev/eyes.ts
+++
@@ -1,8 +0,0 @@
-class Eyes {
-    eyes : HTMLElement
-    constructor(x: number, y: number, scale: number) {
-        this.eyes = document.createElement("eyes")
-    document.body.appendChild(this.eyes)
-    this.eyes.style.transform = `translate(${x}px, ${y}px) scale(${scale})`
-    }
-}
