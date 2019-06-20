45734f06aca133c0255dd47262be15713c9e60b6
jesseduijndam <50326150+jesseduijndam@users.noreply.github.com>
2019-06-20 18:37:11 +0200

    naam sign ding

--- dev/sign.ts
+++ dev/sign.ts
@@ -7,11 +7,16 @@ class Sign{
             document.body.appendChild(this.sign)
             this.sign.style.transform = `translate(${x}px, ${y}px) scale(${scale})`
             console.log("signcreated");
-        } else{
+        } else if(type == 1){
             this.bord = document.createElement("bord")
             document.body.appendChild(this.bord)
             this.bord.style.transform = `translate(${x}px, ${y}px) scale(${scale})`
             console.log("bordcreated");
-        }                   
+        } else if(type == 2){
+            this.bord = document.createElement("back2shop")
+            document.body.appendChild(this.bord)
+            this.bord.style.transform = `translate(${x}px, ${y}px) scale(${scale})`
+            console.log("back2shop created");
+        }             
     }
 }
